8beea931d126f27a220c56f321d74f8e97420485
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-10-10 16:17:02 -0400

    init project

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/Routes';
+
+export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Routes />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
