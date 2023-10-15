279b96a73ec24e819730316c4fe9f299f01615bb
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-10-15 16:45:32 -0400

    refactor: api url

--- src/api.tsx
+++ src/api.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_URL = 'https://eduardo-gimenes-sds2.herokuapp.com';
+const API_URL = "https://dsdelivery-api.onrender.com";
 
 export function fetchOrders() {
-    return axios(`${API_URL}/orders`);
+  return axios(`${API_URL}/orders`);
 }
 
 export function confirmDelivery(orderID: number) {
-    return axios.put(`${API_URL}/orders/${orderID}/delivered`);
+  return axios.put(`${API_URL}/orders/${orderID}/delivered`);
 }
